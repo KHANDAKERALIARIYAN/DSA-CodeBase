9652c6d833d9625764341d750c8c4d6691620ae1
Khandaker Ali Ariyan <khandakeraliariyanndc@gmail.com>
2025-02-23 14:12:58 +0600

    Add files via upload

---
+++ Sorting/allSortingAlgo.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+using namespace std;
+
+void printArray(int a[],int n){
+    for (int i = 0; i < n; i++){
+        cout<<a[i]<<" ";
+    }
+    cout<<endl;
+}
+
+void bubbleSort(int a[],int n){
+    for (int i = 0; i < n; i++){
+        bool swapped =false;
+        for (int j = 0; j < n-1-i; j++){
+            if (a[j]>a[j+1]){
+                swap(a[j],a[j+1]);
+                swapped=true;
+            }           
+        }
+        if(!swapped){
+            return;
+        }        
+    }   
+}
+
+void selectionSort(int a[],int n){
+    for (int i = 0; i < n; i++){
+        int small=i;
+        for (int j = i+1; j < n; j++){
+            if(a[j]<a[small]){
+                small=j;
+            }
+        } 
+        swap(a[i],a[small]);      
+    }  
+}
+
+int main(){
+
+    int a[]={4,7,2,8,9,76,43,111,1,89};
+    int n=sizeof(a)/sizeof(int);
+    cout<<"Before sorting"<<endl;
+    printArray(a,n);
+    cout<<endl;
+
+    // bubbleSort(a,n); 
+    selectionSort(a,n);   
+    cout<<"After sorting"<<endl;    
+    printArray(a,n);
+    
+    return 0;
+}
