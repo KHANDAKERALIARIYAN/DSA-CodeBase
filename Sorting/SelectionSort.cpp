122b6be1a400c0671dae8c29d697bfa9be715df8
Khandaker Ali Ariyan <khandakeraliariyanndc@gmail.com>
2025-02-23 12:38:14 +0600

    Update SelectionSort.cpp

--- Sorting/SelectionSort.cpp
+++ Sorting/SelectionSort.cpp
@@ -1,6 +1,36 @@
 #include<iostream>
 using namespace std;
+
+void printArray(int a[], int n){
+    for (int i = 0; i < n; i++){
+        cout << a[i] << " ";
+    }
+    cout << endl;
+}
+
+void selectionSort(int a[], int n){
+    for (int i = 0; i < n; i++){
+        int smallestIndex=i;
+        for (int j =i+ 1; j < n; j++){
+            if (a[j]<a[smallestIndex]){
+                smallestIndex=j;
+            }         
+        }
+        swap(a[i],a[smallestIndex]);        
+    }   
+}
+
 int main(){
+    int a[] = {4, 7, 2, 8, 9, 76, 43, 111, 1, 89};
+    int n = sizeof(a) / sizeof(int);
+    cout << "Before sorting" << endl;
+    printArray(a, n);
+    cout << endl;
+
+    selectionSort(a, n);
+ 
+    cout << "After sorting" << endl;
+    printArray(a, n);
 
     return 0;
 }
