ae366638c19ed848b0fe2030490332cee9b2bd64
Khandaker Ali Ariyan <khandakeraliariyanndc@gmail.com>
2025-02-23 15:04:58 +0600

    Update allSortingAlgo.cpp

--- Sorting/allSortingAlgo.cpp
+++ Sorting/allSortingAlgo.cpp
@@ -35,6 +35,20 @@ void selectionSort(int a[],int n){
     }  
 }
 
+void insertionSort(int a[],int n){
+    for(int i=1;i<n;i++){
+        int current=a[i];
+        int previous=i-1;
+
+        while(previous>=0 && a[previous]>current){
+            a[previous+1]=a[previous];
+            previous--;
+        }
+
+        a[previous+1]=current;
+    }
+}
+
 int main(){
 
     int a[]={4,7,2,8,9,76,43,111,1,89};
@@ -44,7 +58,8 @@ int main(){
     cout<<endl;
 
     // bubbleSort(a,n); 
-    selectionSort(a,n);   
+    // selectionSort(a,n); 
+    insertionSort(a,n);
     cout<<"After sorting"<<endl;    
     printArray(a,n);
     
