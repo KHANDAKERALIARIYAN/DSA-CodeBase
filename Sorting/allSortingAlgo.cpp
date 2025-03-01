7712ea5c407eac5ddf80c74cd94f92828f683893
Khandaker Ali Ariyan <khandakeraliariyanndc@gmail.com>
2025-03-02 01:58:20 +0600

    Update allSortingAlgo.cpp

--- Sorting/allSortingAlgo.cpp
+++ Sorting/allSortingAlgo.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 
 void printArray(int a[],int n){
@@ -52,7 +53,7 @@ void insertionSort(int a[],int n){
     }
 }
 
-// quick sort
+// quick sort using array
 int partition_array(int a[],int low,int high){
     int pivot=a[high];
     int i=low-1;
@@ -66,6 +67,8 @@ int partition_array(int a[],int low,int high){
     return i+1;
 }
 
+// quick sort using vector
+
 void quickSort_array(int a[],int low, int high){
     if (low<high){
         int pivot=partition_array(a,low,high);
@@ -74,20 +77,89 @@ void quickSort_array(int a[],int low, int high){
     }
 }
 
+int partition(vector<int> &arr,int startingIndex,int endingIndex){
+    int index=startingIndex-1;
+    int pivot=arr[endingIndex];
+    for (int i = startingIndex; i < endingIndex; i++){
+        if (arr[i]<=pivot){
+            index++;
+            swap(arr[i],arr[index]);
+        }
+    }
+    index++;
+    swap(arr[endingIndex],arr[index]);
+    return index;
+}
+
+void quickSort(vector<int> &arr,int startingIndex,int endingIndex){
+    if (startingIndex<endingIndex){
+        int pivotIndex=partition(arr,startingIndex,endingIndex);
+        quickSort(arr,startingIndex,pivotIndex-1); //left half
+        quickSort(arr,pivotIndex+1,endingIndex);  //right half
+    }  
+}
+
+// merge sort
+void merge(vector<int>& arr,int st,int mid,int end){
+    vector<int> temp;
+    int i=st;
+    int j=mid+1;
+    while (i<=mid && j<=end){
+        if (arr[i]<arr[j]){
+            temp.push_back(arr[i]);
+            i++;
+        }
+        else{
+            temp.push_back(arr[j]);
+            j++;
+        }     
+    }
+
+    while (i<=mid){
+        temp.push_back(arr[i]);
+        i++;
+    }
+
+    while (j<=end){
+        temp.push_back(arr[j]);
+        j++;
+    }
+    
+    for (int k = 0; k < temp.size(); k++){
+        arr[st+k]=temp[k];
+    }
+       
+}
+
+void mergeSort(vector<int>& arr,int st,int end){
+    if (st<end){
+        int mid=st+(end-st)/2;
+        mergeSort(arr,st,mid); // left half
+        mergeSort(arr,mid+1,end); // right half
+        merge(arr,st,mid,end);
+    }   
+}
+
 int main(){
 
-    int a[]={4,7,2,8,9,76,43,111,1,89};
-    int n=sizeof(a)/sizeof(int);
-    cout<<"Before sorting"<<endl;
-    printArray(a,n);
-    cout<<endl;
-
+    // int a[]={4,7,2,8,9,76,43,111,1,89};
+    // int n=sizeof(a)/sizeof(int);
+    // cout<<"Before sorting"<<endl;
+    // printArray(a,n);
+    // cout<<endl;
     // bubbleSort(a,n); 
     // selectionSort(a,n); 
     // insertionSort(a,n);
-    quickSort_array(a,0,n-1)
-    cout<<"After sorting"<<endl;    
-    printArray(a,n);
-    
+    // quickSort_array(a,0,n-1)
+    // cout<<"After sorting"<<endl;    
+    // printArray(a,n);
+
+    // vector<int>a={12,31,35,8,32,17};
+    // quickSort(arr,0,arr.size()-1);
+    // mergeSort(a,0,a.size()-1);
+    // for (int val : a){
+    //     cout<<val<<" ";
+    // }
+    cout<<endl;
     return 0;
 }
